efdb7da25ab06f451f8c6f36e6d2ccb9eb7fd3e7
Daniel Funke <daniel.funke@cern.ch>
2014-11-18 15:46:35 +0100

    inCircle predicate

--- src/Geometry.h
+++ src/Geometry.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <iostream>
+#include <set>
 
 #include "Logger.h"
 #include "IdVector.hxx"
@@ -41,9 +42,9 @@ std::ostream & operator<<(std::ostream & o, const Point3D & p);
 //dimensionality of our problem
 const uint D = 2;
 
-struct dPoint {
-	uint id;
-	std::array<tCoordinate, D> coords;
+class dPoint {
+
+public:
 
 	bool operator==(const dPoint & a) const {
 
@@ -77,6 +78,14 @@ struct dPoint {
 		return isFinite(id);
 	}
 
+public:
+
+	uint id;
+	std::array<tCoordinate, D> coords;
+	std::set<uint> simplices;
+
+public:
+
 	static inline bool isFinite(const uint & i) {
 		//INDENT
 		//PLOG << i << " -> " << (i & cINF) << " != " << cINF << ": " << !((i & cINF) == cINF) << std::endl;
@@ -91,10 +100,9 @@ typedef IdVector<dPoint> dPoints;
 typedef std::vector<uint> dPointIds;
 typedef std::vector<dPointIds> Partition;
 
-struct dSimplex {
-	uint id;
-	std::array<uint, D+1> vertices;
-	std::array<uint, D+1> neighbors;
+class dSimplex {
+
+public:
 
 	bool operator==(const dSimplex & a) const {
 
@@ -169,6 +177,60 @@ struct dSimplex {
 
 		return finite;
 	}
+
+	/*
+	 * return > 0: abc are counter-clockwise
+	 * return < 0: abc clockwise
+	 */
+	tCoordinate orientation(const dPoints & points) const {
+	  tCoordinate acx, bcx, acy, bcy;
+
+	  acx = points[vertices[0]].coords[0] - points[vertices[2]].coords[0];
+	  bcx = points[vertices[1]].coords[0] - points[vertices[2]].coords[0];
+	  acy = points[vertices[0]].coords[1] - points[vertices[2]].coords[1];
+	  bcy = points[vertices[1]].coords[1] - points[vertices[2]].coords[1];
+	  return acx * bcy - acy * bcx;
+	}
+
+	bool inCircle(const dPoint & p, const dPoints & points) const {
+	  tCoordinate adx, ady, bdx, bdy, cdx, cdy;
+	  tCoordinate abdet, bcdet, cadet;
+	  tCoordinate alift, blift, clift;
+	  tCoordinate det;
+
+	  adx = points[vertices[0]].coords[0] - p.coords[0];
+	  ady = points[vertices[0]].coords[1] - p.coords[1];
+	  bdx = points[vertices[1]].coords[0] - p.coords[0];
+	  bdy = points[vertices[1]].coords[1] - p.coords[1];
+	  cdx = points[vertices[2]].coords[0] - p.coords[0];
+	  cdy = points[vertices[2]].coords[1] - p.coords[1];
+
+	  abdet = adx * bdy - bdx * ady;
+	  bcdet = bdx * cdy - cdx * bdy;
+	  cadet = cdx * ady - adx * cdy;
+	  alift = adx * adx + ady * ady;
+	  blift = bdx * bdx + bdy * bdy;
+	  clift = cdx * cdx + cdy * cdy;
+
+	  /*
+	   * det > 0: d inside  abc (abc counter-clockwise)
+	   * 		  d outside abc (abc clockwise)
+	   * det < 0: d outside abc (abc counter-clockwise)
+	   * 		  d inside  abc (abc clockwise)
+	   */
+
+	  det = alift * bcdet + blift * cadet + clift * abdet;
+
+	  return orientation(points) * det >= 0;
+	}
+
+public:
+	uint id;
+	std::array<uint, D+1> vertices;
+	std::array<uint, D+1> neighbors;
+
+
+public:
 
 	static bool isFinite(const uint & i){
 		return i != cINF;
