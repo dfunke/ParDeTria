48379a460de6a7ad5aa5d7017d37d11ba2a7ee48
Daniel Funke <daniel.funke@cern.ch>
2014-12-09 15:57:25 +0100

    better detection of headers

--- src/.ycm_extra_conf.py
+++ src/.ycm_extra_conf.py
@@ -139,20 +139,14 @@ def FlagsForFile( filename, **kwargs ):
     # Bear in mind that compilation_info.compiler_flags_ does NOT return a
     # python list, but a "list-like" StringVec object
     compilation_info = GetCompilationInfoForFile( filename )
-    if not compilation_info:
-      return None
+    if compilation_info:
+        final_flags = MakeRelativePathsInFlagsAbsolute(
+          compilation_info.compiler_flags_,
+          compilation_info.compiler_working_dir_ )
+    else:
+        relative_to = DirectoryOfThisScript()
+        final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
 
-    final_flags = MakeRelativePathsInFlagsAbsolute(
-      compilation_info.compiler_flags_,
-      compilation_info.compiler_working_dir_ )
-
-    # NOTE: This is just for YouCompleteMe; it's highly likely that your project
-    # does NOT need to remove the stdlib flag. DO NOT USE THIS IN YOUR
-    # ycm_extra_conf IF YOU'RE NOT 100% SURE YOU NEED IT.
-    try:
-      final_flags.remove( '-stdlib=libc++' )
-    except ValueError:
-      pass
   else:
     relative_to = DirectoryOfThisScript()
     final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
