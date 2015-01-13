65ca07e396667ec16dacbd769884b0e1c891c267
Daniel Funke <daniel.funke@cern.ch>
2015-01-13 16:44:56 +0100

    restructure plotting

--- plot/plotDAC.py
+++ plot/plotDAC.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 from scipy.stats import norm
 
 import matplotlib.pylab as plt
@@ -51,27 +50,44 @@ dir = 'data'
 # data format
 # n splitter provenance base_case edge_tria valid nPoints nSimplices nEdgePoints nEdgeSimplices time mem max_mem
 
+# define some aux data for labeling
+
+class LabeledItem:
+
+    def __init__(self, tag, label, color, marker):
+        self.tag = tag
+        self.label = label
+        self.color = color
+        self.marker = marker
+
+splitters = [  LabeledItem(b'0', '0', 'b', 's')
+             , LabeledItem(b'1', '1', 'g', '^')
+             , LabeledItem(b'c', 'cycle', 'y', 'D')
+             , LabeledItem(b'd', 'two-way', 'r', 'o')
+            ]
 
 print("Reading in data")
 data = np.genfromtxt(os.path.join(dir, 'triangulation_report.csv'), dtype=None, names=True)
 
 # validity
-
+# check for invalid triangulations, only provenance '0' is of concern, as this is the entire triangulation
 fig, ax = getFig("Validity", r"valid", r"")
 
-ax.hist(data['valid'])
+ldata = data[data['provenance'] == b'0']
+bins, _, _ = ax.hist(ldata['valid'], bins=2)
+
+ax.text(0.1, 0.1, 'invalid = %i' %  bins[0], color='white')
+ax.text(0.6, 0.1, 'valid = %i' % bins[1], color='white')
 
 closeFig("report/validity")
 
-#setup unique data
-splitters = np.unique(data['splitter'])
-
 # plot edge size
+# only non base case triangulations are relevant
 fig, ax = getFig("Edge Size", r"$n$", r"edge points")
 
 for s in splitters:
-    ldata = data[data['splitter'] == s]
-    ax.plot(ldata['nPoints'], ldata['nEdgePoints'], 's', label=s)
+    ldata = data[(data['splitter'] == s.tag) & (data['base_case'] == 0)]
+    ax.plot(ldata['nPoints'], ldata['nEdgePoints'], label=s.label, color=s.color, marker=s.marker, ls='None')
 
 ax.legend()
 
@@ -81,9 +97,44 @@ closeFig("report/edgePoints")
 fig, ax = getFig("Edge Size", r"$n$", r"edge simplices")
 
 for s in splitters:
-    ldata = data[data['splitter'] == s]
-    ax.plot(ldata['nPoints'], ldata['nEdgeSimplices'], 's', label=s)
+    ldata = data[(data['splitter'] == s.tag) & (data['base_case'] == 0)]
+    ax.plot(ldata['nPoints'], ldata['nEdgeSimplices'], label=s.label, color=s.color, marker=s.marker, ls='None')
 
 ax.legend()
 
 closeFig("report/edgeSimplices")
+
+# plot runtime
+fig, ax = getFig("Runtime", r"$n$", r"$t$")
+
+for s in splitters:
+    ldata = data[data['splitter'] == s.tag]
+    ax.plot(ldata['n'], ldata['time'], label=s.label, color=s.color, marker=s.marker, ls='None')
+
+ax.legend()
+
+closeFig("report/runtime")
+
+# plot runtime scaled
+fig, ax = getFig("Runtime", r"$n$", r"$\frac{t}{n \log n}$")
+
+for s in splitters:
+    ldata = data[data['splitter'] == s.tag]
+    ax.plot(ldata['n'], ldata['time'] / (ldata['n'] * np.log2(ldata['n'])), label=s.label, color=s.color, marker=s.marker, ls='None')
+
+ax.legend()
+
+closeFig("report/runtime_scaled")
+
+# plot mem
+fig, ax = getFig("Memory", r"$n$", r"$mem$")
+
+for s in splitters:
+    ldata = data[data['splitter'] == s.tag]
+    ax.plot(ldata['n'], ldata['mem'], label=s.label, color=s.color, marker=s.marker, ls='None')
+
+ax.legend()
+
+closeFig("report/memory")
+
+print("Done")
