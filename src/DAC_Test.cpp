bb91ab914d7937184baa292354db8e8238c7f479
Daniel Funke <daniel.funke@cern.ch>
2015-02-06 11:11:36 +0100

    use command line flag for study mode instead of compiler flag

--- src/DAC_Test.cpp
+++ src/DAC_Test.cpp
@@ -3,6 +3,7 @@
 #include "Geometry.h"
 #include "Triangulator.h"
 #include "Partitioner.h"
+#include "Painter.h"
 
 #include "utils/Random.h"
 #include "utils/Logger.h"
@@ -92,7 +93,65 @@ void testGeometry3D() {
 //**************************
 
 #define D 3
-const uint N = 1e3;
+
+//**************************
+
+struct TriangulateReturn {
+  TriangulationReport tr;
+  bool exception;
+  tDuration time;
+};
+
+//**************************
+
+TriangulateReturn triangulate(const dBox<D> &bounds, dPoints<D> &points,
+                              const unsigned char splitter) {
+  std::unique_ptr<Partitioner<D>> partitioner_ptr;
+  switch (splitter) {
+  case 'd':
+    partitioner_ptr = std::make_unique<dPartitioner<D>>();
+    break;
+  case 'c':
+    partitioner_ptr = std::make_unique<CyclePartitioner<D>>();
+    break;
+  default:
+    // p must be a dimension - subtract '0' to get integer value
+    uint d = splitter - '0';
+    ASSERT(0 <= d && d < D);
+    partitioner_ptr = std::make_unique<kPartitioner<D>>(d);
+    break;
+  }
+
+  Triangulator<D> triangulator(bounds, points, std::move(partitioner_ptr));
+
+  TriangulateReturn ret;
+
+  try {
+
+    auto t1 = Clock::now();
+    auto dt = triangulator.triangulate();
+    auto t2 = Clock::now();
+
+    ret.tr = triangulator.getTriangulationReport();
+    ret.exception = false;
+    ret.time = std::chrono::duration_cast<tDuration>(t2 - t1);
+
+  } catch (AssertionException &e) {
+    std::cerr << "Assertion failed - ABORTING - n =  " << points.size()
+              << " p = " << splitter << std::endl;
+    std::cerr << e.what() << std::endl;
+
+    // output points
+    storeObject(points, "assertionFailed_" + std::to_string(points.size()) +
+                            "_" + (char)splitter + ".dat");
+
+    ret.exception = true;
+  }
+
+  return ret;
+}
+
+//**************************
 
 int main(int argc, char *argv[]) {
 
@@ -100,11 +159,12 @@ int main(int argc, char *argv[]) {
 
   namespace po = boost::program_options;
 
-#ifdef STUDY
-  int verbosity = 0;
-#else
   int verbosity = -1;
-#endif
+
+  unsigned char p = 'd';
+  uint N = 1e3;
+
+  bool study = false;
 
   std::string pointFile;
   bool loadPoints = false;
@@ -113,6 +173,12 @@ int main(int argc, char *argv[]) {
   bool loadSimplices = false;
 
   po::options_description cCommandLine("Command Line Options");
+  cCommandLine.add_options()("n", po::value<uint>(&N), "number of points");
+  cCommandLine.add_options()(
+      "splitter", po::value<unsigned char>(&p),
+      "splitter - _c_ycle, _d_-dimensional, _[0-d-1]_ fixed dimension");
+  cCommandLine.add_options()("study", "study mode");
+  cCommandLine.add_options()("no-verify", "don't verify triangulation");
   cCommandLine.add_options()("verbosity", po::value<int>(&verbosity),
                              "verbosity");
   cCommandLine.add_options()("points", po::value<std::string>(&pointFile),
@@ -141,6 +207,14 @@ int main(int argc, char *argv[]) {
     loadSimplices = true; // filename will be in simplexFile
   }
 
+  if (vm.count("study")) {
+    study = true;
+  }
+
+  if (vm.count("no-verify")) {
+    Triangulator<D>::VERIFY = false;
+  }
+
   //***************************************************************************
 
   dBox<D> bounds;
@@ -152,140 +226,112 @@ int main(int argc, char *argv[]) {
   std::uniform_real_distribution<tCoordinate> distribution(0, 1);
   std::function<tCoordinate()> dice = std::bind(distribution, generator);
 
-/*
- * File format for triangulation report
- * input_n splitter provenance base_case edge_triangulation nPoints
- * nSimplices
- * nEdgePoints nEdgeSimplices
- */
+  /*
+   * File format for triangulation report
+   * input_n splitter provenance base_case edge_triangulation nPoints
+   * nSimplices
+   * nEdgePoints nEdgeSimplices
+   */
 
-#ifdef STUDY
-  // define splitter test cases
-  std::vector<unsigned char> splitters = {'d', 'c', '0', '1'};
-  // silence std::cout until STUDY is done - preserve log level
-  LOGGER.setLogLevel(Logger::abs(LOGGER.getLogLevel()));
+  if (study) {
+    // define splitter test cases
+    std::vector<unsigned char> splitters = {'d', 'c', '0', '1'};
 
-  // abort flag if an exception occurred
-  bool abort = false;
-
-  boost::progress_display progress(splitters.size() * (9 * (log10(N) - 1) + 1),
-                                   std::cout);
-
-  std::ofstream f("triangulation_report.csv", std::ios::out | std::ios::trunc);
-  f << CSV::csv("n", "splitter", "provenance", "base_case", "edge_tria",
-                "valid", "nPoints", "nSimplices", "nEdgePoints",
-                "nEdgeSimplices", "time", "mem", "max_mem") << std::endl;
-
-  for (uint n = 10; n <= N && !abort; n += pow(10, floor(log10(n)))) {
-    for (uint i = 0; i < splitters.size() && !abort; ++i) {
-
-      unsigned char p = splitters[i];
-#else  // STUDY
-  unsigned char p = '1';
-  uint n = N;
-#endif // STUDY
-
-      dPoints<D> points;
-      if (loadPoints) {
-        points = loadObject<dPoints<D>>(pointFile);
-      } else {
-        points = genPoints(n, bounds, dice);
-      }
-
-      std::unique_ptr<Partitioner<D>> partitioner_ptr;
-      switch (p) {
-      case 'd':
-        partitioner_ptr = std::make_unique<dPartitioner<D>>();
-        break;
-      case 'c':
-        partitioner_ptr = std::make_unique<CyclePartitioner<D>>();
-        break;
-      default:
-        // p must be a dimension - subtract '0' to get integer value
-        uint d = p - '0';
-        ASSERT(0 <= d && d < D);
-        partitioner_ptr = std::make_unique<kPartitioner<D>>(d);
-        break;
-      }
-
-      Triangulator<D> triangulator(bounds, points, std::move(partitioner_ptr));
-
-      Clock::time_point t1, t2;
-      INDENT
-#ifdef STUDY
-      try {
-#endif
-
-        t1 = Clock::now();
-        auto dt = triangulator.triangulate();
-        t2 = Clock::now();
-
-#ifdef STUDY
-      } catch (AssertionException e) {
-        std::cerr << "Assertion failed - ABORTING - n =  " << n << " p = " << p
-                  << std::endl;
-        std::cerr << e.what() << std::endl;
-
-        // output points
-        storeObject(points, "assertionFailed_" + std::to_string(n) + "_" +
-                                (char)p + ".dat");
-
-        // set abort flag
-        // abort = true;
-      }
-#endif
-
-      LOG << "Triangulating " << points.size() << " points took "
-          << std::chrono::duration_cast<std::chrono::seconds>(t2 - t1).count()
-          << " s" << std::endl;
-      DEDENT
-
-#ifdef STUDY
-      // evaluate the triangulation report
-      const auto &trReport = triangulator.getTriangulationReport();
-
-      for (const auto &tr : trReport) {
-        f << CSV::csv(points.size(), p, tr.provenance, tr.base_case,
-                      tr.edge_triangulation, tr.valid, tr.nPoints,
-                      tr.nSimplices, tr.nEdgePoints, tr.nEdgeSimplices,
-                      std::chrono::duration_cast<tDuration>(t2 - t1).count(),
-                      getCurrentRSS(), getPeakRSS()) << std::endl;
-
-        if (triangulator.getVerify() && Triangulator<D>::isTOP(tr.provenance) &&
-            !tr.valid) {
-
-          // output points
-          storeObject(points,
-                      "invalid_" + std::to_string(n) + "_" + (char)p + ".dat");
-        }
-      }
-
-      ++progress;
+    if (vm.count("verbosity")) {
+      // silence std::cout until STUDY is done - preserve log level
+      LOGGER.setLogLevel(Logger::abs(LOGGER.getLogLevel()));
+    } else {
+      LOGGER.setLogLevel(Logger::Verbosity::SILENT);
     }
-  }
 
-  std::cout << LOGGER << std::endl;
-#else  // STUDY
-  LOG << std::endl << "Triangulation report: " << std::endl;
+    Painter<D>::ENABLED = false;
 
-  const auto &trReport = triangulator.getTriangulationReport();
-  LOG << CSV::csv("n", "splitter", "provenance", "base_case", "edge_tria",
+    // abort flag if an exception occurred
+    bool abort = false;
+
+    boost::progress_display progress(
+        splitters.size() * (9 * (log10(N) - 1) + 1), std::cout);
+
+    std::ofstream f("triangulation_report.csv",
+                    std::ios::out | std::ios::trunc);
+    f << CSV::csv("n", "splitter", "provenance", "base_case", "edge_tria",
                   "valid", "nPoints", "nSimplices", "nEdgePoints",
                   "nEdgeSimplices", "time", "mem", "max_mem") << std::endl;
-  for (const auto &tr : trReport) {
-    LOG << CSV::csv(points.size(), p, tr.provenance, tr.base_case,
-                    tr.edge_triangulation, tr.valid, tr.nPoints, tr.nSimplices,
-                    tr.nEdgePoints, tr.nEdgeSimplices,
-                    std::chrono::duration_cast<tDuration>(t2 - t1).count(),
-                    getCurrentRSS(), getPeakRSS()) << std::endl;
+
+    for (uint n = 10; n <= N && !abort; n += pow(10, floor(log10(n)))) {
+      for (uint i = 0; i < splitters.size() && !abort; ++i) {
+
+        unsigned char p = splitters[i];
+
+        dPoints<D> points;
+        if (loadPoints) {
+          points = loadObject<dPoints<D>>(pointFile);
+        } else {
+          points = genPoints(n, bounds, dice);
+        }
+
+        auto ret = triangulate(bounds, points, p);
+
+        if (ret.exception) {
+          abort = true;
+        }
+
+        // evaluate the triangulation report
+
+        for (const auto &tr : ret.tr) {
+          f << CSV::csv(points.size(), p, tr.provenance, tr.base_case,
+                        tr.edge_triangulation, tr.valid, tr.nPoints,
+                        tr.nSimplices, tr.nEdgePoints, tr.nEdgeSimplices,
+                        ret.time.count(), getCurrentRSS(), getPeakRSS())
+            << std::endl;
+
+          if (Triangulator<D>::VERIFY &&
+              Triangulator<D>::isTOP(tr.provenance) && !tr.valid) {
+
+            // output points
+            storeObject(points, "invalid_" + std::to_string(n) + "_" + (char)p +
+                                    ".dat");
+          }
+        }
+
+        ++progress;
+      }
+    }
+
+    std::cout << LOGGER << std::endl;
+
+    LOG << "Finished" << std::endl;
+
+    return abort ? EXIT_FAILURE : EXIT_SUCCESS;
+
+  } else {
+
+    dPoints<D> points;
+    if (loadPoints) {
+      points = loadObject<dPoints<D>>(pointFile);
+    } else {
+      points = genPoints(N, bounds, dice);
+    }
+
+    auto ret = triangulate(bounds, points, p);
+
+    LOG << "Triangulating " << points.size() << " points took "
+        << std::chrono::duration_cast<std::chrono::seconds>(ret.time).count()
+        << " s" << std::endl;
+
+    VLOG << std::endl << "Triangulation report: " << std::endl;
+
+    VLOG << CSV::csv("n", "splitter", "provenance", "base_case", "edge_tria",
+                     "valid", "nPoints", "nSimplices", "nEdgePoints",
+                     "nEdgeSimplices", "time", "mem", "max_mem") << std::endl;
+    for (const auto &tr : ret.tr) {
+      VLOG << CSV::csv(points.size(), p, tr.provenance, tr.base_case,
+                       tr.edge_triangulation, tr.valid, tr.nPoints,
+                       tr.nSimplices, tr.nEdgePoints, tr.nEdgeSimplices,
+                       ret.time.count(), getCurrentRSS(), getPeakRSS())
+           << std::endl;
+    }
+
+    return EXIT_SUCCESS;
   }
-#endif // STUDY
-
-  LOG << "Finished" << std::endl;
-
-#ifdef STUDY
-  return abort ? EXIT_FAILURE : EXIT_SUCCESS;
-#else
-  return EXIT_SUCCESS;
-#endif
 }
