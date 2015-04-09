423a2bd8f4e1f2d14c732aaa4484188a54a10406
Daniel Funke <daniel.funke@cern.ch>
2015-04-09 14:27:33 +0200

    factory for triangulators

--- include/Triangulator.h
+++ include/Triangulator.h
@@ -19,6 +19,9 @@ public:
         return this->_triangulate(allPoints(), baseBounds, std::to_string(TOP));
     };
 
+    template<typename... Args>
+    static std::unique_ptr<Triangulator<D, Precision>> make(const unsigned char type, Args &... args);
+
 protected:
     virtual dSimplices<D, Precision> _triangulate(const Ids & partitionPoints,
                                          const dBox<D, Precision> &bounds,
@@ -40,3 +43,28 @@ public:
 protected:
     static constexpr char TOP = 0;
 };
+
+// forward declare triangulator types
+template <uint D, typename Precision> class DCTriangulator;
+template <uint D, typename Precision, bool Parallel> class CGALTriangulator;
+
+template<uint D, typename Precision> template <typename... Args>
+std::unique_ptr<Triangulator<D, Precision>> Triangulator<D, Precision>::make
+        (const unsigned char type, Args &... args) {
+
+    std::unique_ptr<Triangulator<D, Precision>> triangulator_ptr;
+    switch (type) {
+        case 'd':
+            triangulator_ptr = std::make_unique<DCTriangulator<D, Precision>>(args...);
+            break;
+        case 'c':
+            triangulator_ptr = std::make_unique<CGALTriangulator<D, Precision, false>>(args...);
+            break;
+        case 'm':
+            triangulator_ptr = std::make_unique<CGALTriangulator<D, Precision, true>>(args...);
+            break;
+    }
+
+    ASSERT(triangulator_ptr);
+    return triangulator_ptr;
+}
