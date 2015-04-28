99996b32b59ae4a3a58fee242ab1565b51b2e35e
Daniel Funke <daniel.funke@cern.ch>
2015-04-28 16:33:16 +0200

    [plot] compare collisions

--- plot/plotWhereUsedStudy.py
+++ plot/plotWhereUsedStudy.py
@@ -52,24 +52,69 @@ rcParams['xtick.direction'] = 'out'
 rcParams['ytick.direction'] = 'out'
 
 dir = 'data'
+files = [ f for f in os.listdir(dir)
+        if f.startswith('whereUsed_collisions_3_c_') and f.endswith('.csv')]
 
-print("Reading in data")
+impl = ['rol', 'xor', 'xorSeed']
+labels =  {'rol' : 'rotate', 'xor' : 'xor', 'xorSeed' : 'xor with seed'}
+colors  = {'rol' : 'blue', 'xor' : 'green', 'xorSeed' : 'red'}
+markers = {'rol' : 'x', 'xor' : 'D', 'xorSeed' : 'o'}
 
-rawData = np.genfromtxt(os.path.join(dir, 'whereUsed_collisions_3_c.csv'), names=True, dtype=int)
-rawData.sort(order='n')
+colData = {}
+for f in files:
+    print("Reading in data from %s" % f)
+    alg = f.replace('whereUsed_collisions_3_c_', '').replace('.csv','')
 
-N = np.unique(rawData['n'])
+    rawData = np.genfromtxt(os.path.join(dir, f), names=True, dtype=int)
+    rawData.sort(order='n')
 
-print("Processing data")
-colData = []
-for n in N:
-    colData.append(rawData[rawData['n'] == n]['collisions'])
+    N = np.unique(rawData['n'])
 
-print("Generating plots")
+    print("Processing data")
+    colData[alg] = []
+    for n in N:
+        colData[alg].append(rawData[rawData['n'] == n]['collisions'])
+
+    print("Generating plots")
+
+    fig, ax = getFig("Collisions over Points", r"$n$", r"$c$")
+
+    ax.boxplot(colData[alg], whis=[5,95],
+               boxprops={'color' : colors[alg]},
+               whiskerprops={'color' : colors[alg]},
+               capprops={'color' : colors[alg]},
+               flierprops={'color' : colors[alg], 'marker' : markers[alg], 'markersize' : .9},
+               medianprops={'color' : 'magenta'})
+
+    ax.set_xlim(0, len(N)+1)
+    ax.set_xticklabels((N-8)[0::9])
+    ax.set_xticks(np.arange(1, len(N)+1, 9))
+
+    _, ymax = ax.get_ylim()
+    ymax += 1
+    ax.set_ylim(0, ymax)
+
+    #ax.legend()
+    fig.text(0.12, 0.9, labels[alg], va='bottom', ha='left')
+
+    closeFig("whereUsed_study/00_collisions_%s" % alg)
+
+print("Generating combined plots")
 
 fig, ax = getFig("Collisions over Points", r"$n$", r"$c$")
 
-ax.boxplot(colData, whis=[5,95])
+start = .75
+legItems = []
+for alg in colData:
+    ax.boxplot(colData[alg], positions = np.arange(start, len(N)+start, 1),
+               widths=.2, whis=[5,95],
+               boxprops={'color' : colors[alg]},
+               whiskerprops={'color' : colors[alg]},
+               capprops={'color' : colors[alg]},
+               flierprops={'color' : colors[alg], 'marker' : markers[alg], 'markersize' : .5},
+               medianprops={'color' : 'magenta'})
+    legItems.append(mpatches.Patch(color=colors[alg], label=labels[alg]))
+    start += .25
 
 ax.set_xlim(0, len(N)+1)
 ax.set_xticklabels((N-8)[0::9])
@@ -79,9 +124,9 @@ _, ymax = ax.get_ylim()
 ymax += 1
 ax.set_ylim(0, ymax)
 
-#ax.legend()
+ax.legend(handles=legItems, bbox_to_anchor=(1.1, 1.1))
 
-closeFig("whereUsed_study/04_collisions")
+closeFig("whereUsed_study/00_collisions_combined")
 
 
 print("Done")
