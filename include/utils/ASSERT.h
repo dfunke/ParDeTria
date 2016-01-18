75df36c75a3350d2ace728e25e87e111eb424b57
dfunke <daniel.funke@cern.ch>
2016-01-18 19:32:02 +0100

    introduce MASSERT

--- include/utils/ASSERT.h
+++ include/utils/ASSERT.h
@@ -19,6 +19,11 @@ public:
             : _what(file + ":" + std::to_string(line) + ": failed assertion `" +
                     expr + "'") { }
 
+    AssertionException(const std::string &expr, const std::string &file,
+                       const uint line, const std::string & msg)
+            : _what(file + ":" + std::to_string(line) + ": failed assertion `" +
+                    expr + "' - " + msg) { }
+
     const char *what() const noexcept { return _what.c_str(); }
 
 private:
@@ -28,12 +33,16 @@ private:
 #ifdef NDEBUG
 
 #define ASSERT(e) ((void)(0))
+#define MASSERT(e, msg) ((void)(0))
 #define RASSERT(e) ((void)(0))
 
 #else // NDEBUG
 
 #define ASSERT(e)                                                              \
   ((void)((e) ? 0 : throw AssertionException(#e, __FILE__, __LINE__)))
+
+#define MASSERT(e, msg)                                                              \
+  ((void)((e) ? 0 : throw AssertionException(#e, __FILE__, __LINE__, msg)))
 
 #define RASSERT(e)                                                              \
   ((void)((e) ? 0 : raise(SIGINT)))
