accab582d4f455b12b8825c85cb3d3ec3b27efb5
Daniel Funke <daniel.funke@kit.edu>
2016-05-02 17:00:42 +0200

    license for Timo

--- src/utils/VTuneAdapter.cpp
+++ src/utils/VTuneAdapter.cpp
@@ -1,6 +1,9 @@
-//
-// Created by dfunke on 5/7/15.
-//
+/*
+Copyright © 2016 Daniel Funke <daniel.funke@kit.edu>
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+*/
 
 #include "VTuneAdapter.h"
 
