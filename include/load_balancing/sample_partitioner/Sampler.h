a2657620b4e7ce5336a5d0132431d83f5df1b45d
Vincent Winkler <vincent.winkler@student.kit.edu>
2018-07-10 23:09:32 +0200

    Fix Sampler

--- include/load_balancing/sample_partitioner/Sampler.h
+++ include/load_balancing/sample_partitioner/Sampler.h
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <limits>
 #include <vector>
+#include <unordered_map>
 #include <kaHIP_interface.h>
 #include "CGALTriangulator.h"
 #include "load_balancing/VectorOperations.h"
@@ -79,11 +80,13 @@ namespace LoadBalancing
         {
             auto sample = generateSample(mSampleSize(points.finite_size()), pointIds, mRand);
             auto simplices = triangulateSample(bounds, points, sample);
-            auto graph = makeGraph(simplices, points, lenSquared(bounds.low - bounds.high));
+            auto idTranslation = generateIdTranslation(sample);
+            auto graph = makeGraph(simplices, points, idTranslation,
+                                   lenSquared(bounds.low - bounds.high));
             auto partition = partitionGraph(graph, partitionSize, mRand);
-	        std::vector<dVector<D, Precision>> samplePointVector;
+	        std::vector<dVector<D, Precision>> samplePointVector(idTranslation.size());
             for(auto id : sample) {
-                samplePointVector.push_back(points[id].coords);
+                samplePointVector[idTranslation[id]] = points[id].coords;
             }
             return Sampling<D, Precision>{std::move(graph), std::move(partition), std::move(samplePointVector), std::move(simplices)};
         }
@@ -95,43 +98,49 @@ namespace LoadBalancing
         std::function<Precision(int)> mEdgeWeight;
         
         template <typename Generator_t>
-        std::vector<tIdType> generateSample(size_t sampleSize, size_t pointCloudSize, size_t offset, Generator_t& rand) {
-            std::uniform_int_distribution<size_t> dist(0, pointCloudSize - 1);
-            
-            std::set<tIdType> sample;
-            while(sample.size() < sampleSize) {
-                sample.insert(offset + dist(rand));
-            }
-            
-            std::vector<tIdType> result;
-            std::copy(sample.begin(), sample.end(), std::back_inserter(result));
-            return result;
-        }
-        
-        template <typename Generator_t>
-        std::vector<tIdType> generateSample(size_t sampleSize, const Point_Ids& pointIds, Generator_t& rand) {
-            
-            std::vector<tIdType> result;
-            std::sample(pointIds.begin(), pointIds.end(), std::back_inserter(result), sampleSize, rand);
-            result.erase(std::remove_if(result.begin(), result.end(), [](tIdType id){
+        Point_Ids generateSample(size_t sampleSize, const Point_Ids& pointIds, Generator_t& rand) {
+			std::vector<tIdType> resultIds;
+            std::sample(pointIds.begin(), pointIds.end(), std::back_inserter(resultIds),
+                        sampleSize, rand);
+            resultIds.erase(std::remove_if(resultIds.begin(), resultIds.end(), [](tIdType id){
                 return !dPoint<D, Precision>::isFinite(id);
                 
             }),
-            result.end());
+            resultIds.end());
+            Point_Ids result;
+            for(auto id : resultIds) {
+	            result.insert(id);
+			}
             return result;
         }
 
         dSimplices<D, Precision> triangulateSample(const dBox<D, Precision>& bounds,
                                                    dPoints<D, Precision>& points,
-                                                   const std::vector<tIdType>& samplePoints) {
+                                                   const Point_Ids& samplePoints) {
             
             CGALTriangulator<D, Precision> triangulator(bounds, points);
-            return triangulator.triangulate(samplePoints);
+            return triangulator.triangulateSome(samplePoints, bounds);
         }
+
+		std::unordered_map<tIdType, size_t> generateIdTranslation(const Point_Ids& ids) {
+			std::unordered_map<tIdType, size_t> result;
+			size_t count = 0;
+			for(auto id : ids) {
+				result[id] = count++;
+			}
+			assert(count == result.size());
+			return result;
+		}
         
         void sanitizeAdjacencyList(std::vector<std::vector<std::pair<size_t, int>>>& adjacencyList) {
 	    	assert(adjacencyList.size() > 0);
-			assert(adjacencyList[0].size() == 0);
+    	    assert((std::all_of(begin(adjacencyList), end(adjacencyList),
+								[&adjacencyList](const auto& adjacentNodes) -> bool {
+								return std::all_of(begin(adjacentNodes), end(adjacentNodes),
+				                       [&adjacencyList](const auto& node) -> bool {
+				                       return node.first < adjacencyList.size();
+				                       });
+								})));
 
             for(size_t i = 0; i < adjacencyList.size(); ++i) {
                 auto& adjacentNodes = adjacencyList[i];
@@ -139,24 +148,25 @@ namespace LoadBalancing
                 std::sort(begin(adjacentNodes), end(adjacentNodes));
                 adjacentNodes.erase(std::unique(begin(adjacentNodes), end(adjacentNodes)), end(adjacentNodes));
 
-		auto compare = [](const std::pair<size_t, int>& left, const std::pair<size_t, int>& right) -> bool {
-		   return left.first < right.first;
-		};
+				auto compare = [](const std::pair<size_t, int>& left, const std::pair<size_t, int>& right) -> bool {
+				   return left.first < right.first;
+				};
                 
-                auto lowerBound = std::lower_bound(begin(adjacentNodes), end(adjacentNodes), std::make_pair(i, 0), compare);
-                auto upperBound = std::upper_bound(begin(adjacentNodes), end(adjacentNodes), std::make_pair(i, 0), compare);
+                auto lowerBound = std::lower_bound(begin(adjacentNodes), end(adjacentNodes),
+												   std::make_pair(i, 0), compare);
+                auto upperBound = std::upper_bound(begin(adjacentNodes), end(adjacentNodes),
+												   std::make_pair(i, 0), compare);
                 assert(std::distance(lowerBound, upperBound) <= 1);
                 adjacentNodes.erase(lowerBound, upperBound);
-                
-		std::transform(begin(adjacentNodes), end(adjacentNodes), begin(adjacentNodes), [](const auto& node) -> auto { return std::make_pair(node.first - 1, node.second); });
 	    }
-	    adjacencyList.erase(adjacencyList.begin());
 
-    	    assert((std::all_of(begin(adjacencyList), end(adjacencyList), [&adjacencyList](const auto& adjacentNodes) -> bool {
-				    return std::all_of(begin(adjacentNodes), end(adjacentNodes), [&adjacencyList](const auto& node) -> bool {
-						    return node.first < adjacencyList.size();
-						    });
-				    })));
+    	    assert((std::all_of(begin(adjacencyList), end(adjacencyList),
+								[&adjacencyList](const auto& adjacentNodes) -> bool {
+								return std::all_of(begin(adjacentNodes), end(adjacentNodes),
+				                       [&adjacencyList](const auto& node) -> bool {
+				                       return node.first < adjacencyList.size();
+				                       });
+								})));
             
         }
 
@@ -178,24 +188,26 @@ namespace LoadBalancing
             return result;
         }
 
-        Graph makeGraph(const dSimplices<D, Precision>& simplices, const dPoints<D, Precision>& samplePoints,
-			Precision maxDistSquared) {
+        Graph makeGraph(const dSimplices<D, Precision>& simplices,
+                        const dPoints<D, Precision>& samplePoints,
+                        std::unordered_map<tIdType, size_t> idTranslation,
+                        Precision maxDistSquared) {
 	        Mapper<Precision, int> map(0.0, maxDistSquared, 1, std::numeric_limits<int>::max());
 
-            std::vector<std::vector<std::pair<size_t, int>>> adjacencyList;
+            std::vector<std::vector<std::pair<size_t, int>>> adjacencyList(idTranslation.size());
             for(auto simplex : simplices) {
                 for(auto pointId : simplex.vertices) {
+	                auto i = idTranslation[pointId];
                     if(dPoint<D, Precision>::isFinite(pointId)) {
-                        if(pointId >= adjacencyList.size()) {
-                            adjacencyList.resize(pointId + 1);
-                        }
 						for(auto id : simplex.vertices){
+							auto j = idTranslation[id];
 							if(dPoint<D, Precision>::isFinite(id)) {
-								const Precision distSquared = lenSquared(samplePoints[pointId].coords - samplePoints[id].coords);
+								const Precision distSquared =
+									lenSquared(samplePoints[i].coords - samplePoints[j].coords);
 								std::pair<tIdType, int> edge;
-								edge.first = id;
+								edge.first = j;
 								edge.second = mUniformEdges ? 1 : map(mEdgeWeight(distSquared));
-								adjacencyList[pointId].push_back(edge);
+								adjacencyList[i].push_back(edge);
 							}
 						}
                     }
