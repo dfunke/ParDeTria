caef85da3697da4e6a61dcdceebc500fd558bdea
Daniel Funke <funke@kit.edu>
2018-09-12 13:14:48 +0200

    add sorting to graph file, add makeGraph_sequential for comparision

--- include/load_balancing/sample_partitioner/Sampler.h
+++ include/load_balancing/sample_partitioner/Sampler.h
@@ -184,83 +184,102 @@ namespace LoadBalancing
 			return result;
 		}
         
-//        void sanitizeAdjacencyList(tParAdjList& adjacencyList) {
-//            assert(adjacencyList.size() > 0);
-//    	    assert((std::all_of(begin(adjacencyList), end(adjacencyList),
-//								[&adjacencyList](const auto& adjacentNodes) -> bool {
-//								return std::all_of(begin(adjacentNodes), end(adjacentNodes),
-//				                       [&adjacencyList](const auto& node) -> bool {
-//				                       return node.first < adjacencyList.size();
-//				                       });
-//								})));
-//
-//            tbb::parallel_for(
-//                    std::size_t(0), adjacencyList.size(),
-//                    [&](const uint i) {
-//                        auto& adjacentNodes = adjacencyList[i];
-//
-//                        auto compare = [](const std::pair<size_t, int>& left,
-//                                          const std::pair<size_t, int>& right) -> bool {
-//                            return left.first < right.first;
-//                        };
-//
-//                        std::sort(begin(adjacentNodes), end(adjacentNodes), compare);
-////                        adjacentNodes.erase(std::unique(begin(adjacentNodes),
-////                                                        end(adjacentNodes)),
-////                                            end(adjacentNodes));
-//                        if(std::unique(begin(adjacentNodes), end(adjacentNodes)) != end(adjacentNodes)){
-//                            for(auto i : adjacentNodes)
-//                                std::cout << i.first << " ";
-//                        }
-//                        std::cout << std::endl;
-//                        RASSERT(std::unique(begin(adjacentNodes), end(adjacentNodes)) == end(adjacentNodes));
-//                    }
-//            );
-//
-////            for(size_t i = 0; i < adjacencyList.size(); ++i) {
-////                auto& adjacentNodes = adjacencyList[i];
-////
-////				auto compare = [](const std::pair<size_t, int>& left,
-////				                  const std::pair<size_t, int>& right) -> bool {
-////				   return left.first < right.first;
-////				};
-////
-////                std::sort(begin(adjacentNodes), end(adjacentNodes), compare);
-////                adjacentNodes.erase(std::unique(begin(adjacentNodes),
-////                                                end(adjacentNodes)),
-////                                    end(adjacentNodes));
-////	    }
-//
-//    	    assert((std::all_of(begin(adjacencyList), end(adjacencyList),
-//								[&adjacencyList](const auto& adjacentNodes) -> bool {
-//								return std::all_of(begin(adjacentNodes), end(adjacentNodes),
-//				                       [&adjacencyList](const auto& node) -> bool {
-//				                       return node.first < adjacencyList.size();
-//				                       });
-//								})));
-//
-//        }
-//
-//        Graph adjacencyListToGraph(const tParAdjList& adjacencyList, const std::size_t & n, const std::size_t & m) {
-//            Graph result(n, m);
-//
-//            int currentRecordBegin = 0;
-//            for(const auto& adjacentPoints : adjacencyList) {
-//                    result.nodeRecords.push_back(currentRecordBegin);
-//                    currentRecordBegin += adjacentPoints.size();
-//
-//                for(auto adjacentPoint : adjacentPoints) {
-//                    result.adjacency.push_back(adjacentPoint.first);
-//				    result.edgeWeights.push_back(adjacentPoint.second);
-//                }
-//            }
-//            result.nodeRecords.push_back(result.adjacency.size());
-//
-//            ASSERT(result.nodeRecords.size() - 1 == n);
-//            ASSERT(result.adjacency.size() == m);
-//
-//            return result;
-//        }
+        void sanitizeAdjacencyList(std::vector<std::vector<std::pair<size_t, int>>>& adjacencyList) {
+            assert(adjacencyList.size() > 0);
+    	    assert((std::all_of(begin(adjacencyList), end(adjacencyList),
+								[&adjacencyList](const auto& adjacentNodes) -> bool {
+								return std::all_of(begin(adjacentNodes), end(adjacentNodes),
+				                       [&adjacencyList](const auto& node) -> bool {
+				                       return node.first < adjacencyList.size();
+				                       });
+								})));
+
+            for(size_t i = 0; i < adjacencyList.size(); ++i) {
+                auto& adjacentNodes = adjacencyList[i];
+
+				auto compare = [](const std::pair<size_t, int>& left,
+				                  const std::pair<size_t, int>& right) -> bool {
+				   return left.first < right.first;
+				};
+
+                std::sort(begin(adjacentNodes), end(adjacentNodes), compare);
+                adjacentNodes.erase(std::unique(begin(adjacentNodes),
+                                                end(adjacentNodes)),
+                                    end(adjacentNodes));
+	    }
+
+    	    assert((std::all_of(begin(adjacencyList), end(adjacencyList),
+								[&adjacencyList](const auto& adjacentNodes) -> bool {
+								return std::all_of(begin(adjacentNodes), end(adjacentNodes),
+				                       [&adjacencyList](const auto& node) -> bool {
+				                       return node.first < adjacencyList.size();
+				                       });
+								})));
+
+        }
+
+        Graph adjacencyListToGraph(const std::vector<std::vector<std::pair<size_t, int>>>& adjacencyList, const std::size_t & n, const std::size_t & m) {
+            Graph result(n, m);
+
+            result.nodeRecords.clear();
+            result.adjacency.clear();
+            result.edgeWeights.clear();
+
+            int currentRecordBegin = 0;
+            for(const auto& adjacentPoints : adjacencyList) {
+                    result.nodeRecords.push_back(currentRecordBegin);
+                    currentRecordBegin += adjacentPoints.size();
+
+                for(auto adjacentPoint : adjacentPoints) {
+                    result.adjacency.push_back(adjacentPoint.first);
+				    result.edgeWeights.push_back(adjacentPoint.second);
+                }
+            }
+            result.nodeRecords.push_back(result.adjacency.size());
+
+            return result;
+        }
+
+        Graph makeGraph_sequential(const dSimplices<D, Precision>& simplices,
+                        const dPoints<D, Precision>& samplePoints,
+                        std::unordered_map<tIdType, size_t> idTranslation,
+                        Precision maxDistSquared) {
+
+            VTUNE_TASK(SampleMakeGraph);
+
+            auto lowerWeight = mUniformEdges ? 1 : mEdgeWeight(0.0);
+            auto upperWeight = mUniformEdges ? 2 : mEdgeWeight(1.0);
+            auto [minWeight, maxWeight] = std::minmax(lowerWeight, upperWeight);
+            Mapper<Precision, int> map(minWeight, maxWeight, 1, 99);
+
+            std::size_t n = idTranslation.size();
+            std::size_t m = 0;
+
+            std::vector<std::vector<std::pair<size_t, int>>> adjacencyList(idTranslation.size());
+            for(auto simplex : simplices) {
+                for(auto pointId : simplex.vertices) {
+                    auto i = idTranslation[pointId];
+                    if(dPoint<D, Precision>::isFinite(pointId)) {
+                        for(auto id : simplex.vertices){
+                            auto j = idTranslation[id];
+                            if(dPoint<D, Precision>::isFinite(id) && i != j) {
+                                const Precision distSquared =
+                                        lenSquared(samplePoints[pointId].coords - samplePoints[id].coords);
+                                const Precision normalizedDistSquared = distSquared / maxDistSquared;
+                                std::pair<tIdType, int> edge;
+                                edge.first = j;
+                                edge.second = mUniformEdges ?
+                                              1 : map(mEdgeWeight(normalizedDistSquared));
+                                adjacencyList[i].push_back(edge);
+                                ++m;
+                            }
+                        }
+                    }
+                }
+            }
+            sanitizeAdjacencyList(adjacencyList);
+            return adjacencyListToGraph(adjacencyList, n, m);
+        }
 
         Graph makeGraph(const dSimplices<D, Precision>& simplices,
                         const dPoints<D, Precision>& samplePoints,
@@ -277,7 +296,7 @@ namespace LoadBalancing
 
             VTUNE_TASK(SampleMakeGraph_COLLECT);
 
-            //tbb::concurrent_unordered_map<std::pair<tIdType, tIdType>, int> edgeMap(D*simplices.upperBound() - simplices.lowerBound());
+//            tbb::concurrent_unordered_map<std::pair<tIdType, tIdType>, int> edgeMap(D*simplices.upperBound() - simplices.lowerBound());
 
             typedef Concurrent_LP_Map<tIdType, int> tEdgeMap;
             typedef GrowingHashTable<tEdgeMap> tgEdgeMap;
@@ -309,9 +328,11 @@ namespace LoadBalancing
                                             lenSquared(samplePoints[pointId].coords - samplePoints[id].coords);
                                     const Precision normalizedDistSquared = distSquared / maxDistSquared;
                                     std::size_t edge = (i << (sizeof(tIdType) * CHAR_BIT / 2)) + j;
+//                                    auto edge = std::make_pair(i, j);
                                     int weight = mUniformEdges ?
                                                   1 : map(mEdgeWeight(normalizedDistSquared));
                                     if(edgeMapHandle.insert(edge, weight)) {
+//                                    if(edgeMap.insert(std::make_pair(edge, weight)).second) {
                                         __atomic_fetch_add(&graph.nodeRecords[i+1], 1, __ATOMIC_RELAXED);
                                     }
 
@@ -331,11 +352,14 @@ namespace LoadBalancing
             VTUNE_TASK(SampleMakeGraph_ASSIGN);
             std::vector<int> index(idTranslation.size());
             tbb::parallel_for(edgeMap.handle().range(), [&] (const auto & r) {
+//            tbb::parallel_for(edgeMap.range(), [&] (const auto & r) {
 
                 for(auto v=r.begin(); v !=r.end(); ++v ){
                     const auto & edge = *v;
                     tIdType i = edge.first >> (sizeof(tIdType) * CHAR_BIT / 2);
                     tIdType j = edge.first - (i << (sizeof(tIdType) * CHAR_BIT / 2));
+//                    tIdType i = edge.first.first;
+//                    tIdType j = edge.first.second;
 
                     assert(0 <= i && i < idTranslation.size());
                     assert(0 <= j && j < idTranslation.size());
@@ -349,29 +373,24 @@ namespace LoadBalancing
                 }
             });
 
-//            for(auto simplex : simplices) {
-//                for(auto pointId : simplex.vertices) {
-//	                auto i = idTranslation[pointId];
-//                    if(dPoint<D, Precision>::isFinite(pointId)) {
-//						for(auto id : simplex.vertices){
-//							auto j = idTranslation[id];
-//							if(dPoint<D, Precision>::isFinite(id) && i != j) {
-//								const Precision distSquared =
-//									lenSquared(samplePoints[pointId].coords - samplePoints[id].coords);
-//								const Precision normalizedDistSquared = distSquared / maxDistSquared;
-//								std::pair<tIdType, int> edge;
-//								edge.first = j;
-//								edge.second = mUniformEdges ?
-//									1 : map(mEdgeWeight(normalizedDistSquared));
-//								adjacencyList[i].push_back(edge);
-//								++m;
-//							}
-//						}
-//                    }
-//                }
-//            }
+            tbb::parallel_for(tbb::blocked_range(std::size_t(0), idTranslation.size()), [&] (const auto &r){
+                for (auto n = r.begin(); n < r.end(); ++n) {
+                    for (int i = graph.nodeRecords[n]; i < graph.nodeRecords[n+1]; ++i) {
+                        auto value = graph.adjacency[i];
+                        auto data = graph.edgeWeights[i];
+                        int hole = i;
 
-            //sanitizeAdjacencyList(adjacencyList);
+                        for (; hole > graph.nodeRecords[n] && value < graph.adjacency[hole - 1]; --hole) {
+                            graph.adjacency[hole] = graph.adjacency[hole - 1];
+                            graph.edgeWeights[hole] = graph.edgeWeights[hole - 1];
+                        }
+
+                        graph.adjacency[hole] = value;
+                        graph.edgeWeights[hole] = data;
+                    }
+                }
+            });
+
             return graph;
         }
 
